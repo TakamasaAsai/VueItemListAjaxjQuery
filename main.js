5f6713ac30b0d2371742a44c3588106c838a9d4a
Takamasa Asai <takamasa.asai@fullspeedtechnologies.com>
2019-11-28 18:38:33 +0800

    first commit

--- main.js
+++ main.js
@@ -1,35 +1,52 @@
-var btnLoad = document.querySelector('#load');
 // 読み込みボタンのクリックイベントハンドラを定義
-btnLoad.addEventListener('click', function(event) {
-  //【手順1】XMLHttpRequestオブジェクトのインスタンスを生成
-  var xmlHttpRequest  = new XMLHttpRequest();
-  //【手順2】通信状態の変化を監視するイベントハンドラを設定
-  xmlHttpRequest.onreadystatechange = function() {
-    // レスポンスの受信が正常に完了したとき
-    if (this.readyState == 4 /*&& this.status == 200*/) {
-      // 受信したJSONを変数に格納する
-      var products = this.response;
-      // 商品リストの子ノードを全て削除する
-      var result  = document.querySelector('#result');
-      result.textContent = '';
-      // 商品の子ノードをDOMに挿入する
-      for (var i=0; i<products.length; i++) {
-        var text = '商品ID:' + products[i].id;
-        text += ' 商品名:' + products[i].name;
-        text += ' 料金:' + products[i].price;
-        text += ' 画像パス:' + products[i].image;
-        text += ' 送料:' + products[i].delv;
-        text += ' セール対象:' + products[i].isSale;
-        var div = document.createElement('div');
-        div.textContent = text;
-        result.appendChild(div);
-      }
-    }
-  };
-  //【手順3】レスポンスの形式を指定する
-  xmlHttpRequest.responseType = 'json';
-  //【手順4】リクエストメソッドと読み込むファイルのパスを指定する
-  xmlHttpRequest.open('GET', 'products.json');
-  //【手順5】リクエストを送信する（非同期通信を開始する）
-  xmlHttpRequest.send();
-});
+$('#load').on('click', clickHandler); 
+function clickHandler(event) {
+  // ここにJSONを読み込む処理を記述する
+  // 非同期通信でJSONを読み込む
+  $.ajax({
+    url : 'products.json',  //通信先URL
+    type: 'GET',             //使用するHTTPメソッド（デフォルトがGETなので省略可能）
+    dataType: 'json'        //レスポンスのデータタイプ 
+  })
+  .done(function(data, textStatus, jqXHR) {
+    // ここに通信成功の処理を記述する
+    updateScreen(data);
+    console.log(jqXHR);
+  })
+  .fail(function(jqXHR, textStatus, errorThrown) {
+    // ここに通信失敗の処理を記述する
+    console.log('通信が失敗しました');
+    console.log(jqXHR);
+  });
+}
+
+// 商品一覧の描画を更新する
+function updateScreen(products) {
+  // 商品リストの子ノードを全て削除する
+  $('#result').empty();
+  // 商品の子ノードをDOMに挿入する
+  var list = '';
+  for (var i = 0; i<products.length; i++) {
+    list += '<div>';
+    list += '商品ID:' + products[i].id;
+    list += '商品名:' + products[i].name;
+    list += '料金:' + products[i].price;
+    list += '画像パス:' + products[i].image;
+    list += '送料:' + products[i].delv;
+    list += 'セール対象:' + products[i].isSale;
+    list += '</div>';
+  }
+  $('#result').append(list);
+}
+
+
+
+
+
+
+
+
+
+
+
+
